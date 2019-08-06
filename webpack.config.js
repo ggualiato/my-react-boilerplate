5612bf511109ea55240eb296008178470f9e17a4
Giovanni Gualiato <giovanni.gualiato@hexagon.com>
2019-08-06 13:10:37 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = (env) => {
+    const isProduction = env === 'production'
+
+    return {
+        entry: './src/app.js',
+        mode: 'development',
+        output: {
+            path: path.join(__dirname, 'public', 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }                   
+                ]
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({ filename: 'styles.css' })
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true,
+            publicPath: '/dist/'
+        }
+    }
+}
