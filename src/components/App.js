33372beb713155cf5a614402f547740f8926bd65
Giovanni Gualiato <giovanni.gualiato@hexagon.com>
2020-08-02 15:51:44 -0300

    simple update to test pre-commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class App extends Component {
+    onClick = () => {
+        alert("Hello World");
+    };
+
     render() {
         return (
             <div className="app">
                 <h3>Hello React- Basic project</h3>
+                <button onClick={this.onClick}>Click here!</button>
             </div>
-        )
+        );
     }
 }
