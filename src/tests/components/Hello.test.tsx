943224b1268a2d6d8e5290c088ce71a4cbbc94d1
Giovanni Gualiato <ggualiato@gmail.com>
2021-09-05 00:16:26 -0300

    Testing Hello components

---
+++ src/tests/components/Hello.test.tsx
@@ -0,0 +1,8 @@
+import { shallow } from "enzyme";
+import React from "react";
+import { Hello } from "../../components/Hello";
+
+test("should render Hello correctly", () => {
+    const wrapper = shallow(<Hello text="Test" onClick={() => undefined} />);
+    expect(wrapper).toMatchSnapshot();
+});
