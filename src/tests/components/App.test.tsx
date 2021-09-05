b7353fd7cc108ab20b99a6853376dafc4c13fe11
Giovanni Gualiato <ggualiato@gmail.com>
2021-09-04 23:56:45 -0300

    Setup Enzyme and Jest for snapshot testing

---
+++ src/tests/components/App.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { App } from "../../components/App";
+
+test("should render App correctly", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+});
