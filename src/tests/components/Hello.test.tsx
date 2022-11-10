33f71d12a0d7557e591ca569b402609c92f0fa18
Giovanni Gualiato <ggualiato@gmail.com>
2022-11-10 00:59:03 +0000

    test: update test

--- src/tests/components/Hello.test.tsx
+++ src/tests/components/Hello.test.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { fireEvent, render } from "@testing-library/react";
 import Hello from "../../components/Hello/Hello";
 
 test("should render Hello correctly", () => {
-    const wrapper = shallow(<Hello text="Test" onClick={() => undefined} />);
-    expect(wrapper).toMatchSnapshot();
+    const onClick = jest.fn();
+    const { container, getByRole } = render(<Hello text="Test" onClick={onClick} />);
+    const button = getByRole("button");
+
+    fireEvent.click(button);
+
+    expect(onClick).toHaveBeenCalled();
+    expect(container).toMatchSnapshot();
 });
